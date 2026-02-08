de29bd644b4f04aa362982ae904018bbb28104a8
Bichos a la fuga <bichosalafuga@gmail.com>
2026-02-08 15:54:21 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🐌 Backend Bichos a la fuga funcionando");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor en puerto", PORT);
+});
